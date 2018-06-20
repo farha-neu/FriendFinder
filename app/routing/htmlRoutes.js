b796802026bbbd48ce8c61b161d39898eb680195
Farha B. Jawed <farhajw@gmail.com>
2018-06-20 15:34:08 -0700

    routing done

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,18 +1,14 @@
-module.exports = (function() {
-    'use strict';
-    var externalRoutes = require('express').Router();
-    var path = require("path");
-    
-    externalRoutes.get('/', function (req, res) {
+var path = require("path");
+
+module.exports = (function(app) {
+    app.get('/', function (req, res) {
          res.sendFile(path.join(__dirname, "../public/home.html"));
     });
-    externalRoutes.get('/survey', function (req, res) {
+    app.get('/survey', function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
-    return externalRoutes;
-})();
-
+});
 
 // A GET Route to /survey which should display the survey page.
 // A default, catch-all route that leads to home.html which displays the home page.
--- server.js
+++ server.js
@@ -11,11 +11,8 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var externalRoutes = require('./app/routing/htmlRoutes');
-app.use('/', externalRoutes);
-
-var externalRoutes2 = require('./app/routing/apiRoutes');
-app.use('/', externalRoutes2);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
